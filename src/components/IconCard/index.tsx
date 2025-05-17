6866056379c3abc401ba0f1a38706c8065e19e7a
LucasSon98 <lucas.s.son98@gmail.com>
2025-05-16 22:23:51 -0300

    IconCard component refactoring

--- src/components/IconCard/index.tsx
+++ src/components/IconCard/index.tsx
@@ -3,23 +3,27 @@ import './index.scss';
 
 interface IconCardProps {
   icon: string;
+  type?: string;
+  title: string;
   text: string;
+  displayBadge?: boolean;
+  badgeCount?: string | number;
   iconWidth?: string;
   iconHeight?: string;
 }
 
-const IconCard: React.FC<IconCardProps> = ({ icon, text, iconWidth, iconHeight }) => {
+const IconCard: React.FC<IconCardProps> = ({ icon, type = 'default', title, text, displayBadge = false, badgeCount, iconWidth, iconHeight }) => {
   return (
-    <div className="kuvia-icon-card">
-      <div className="kuvia-icon-card-top">
-        <img src={icon} alt={text} style={{ width: iconWidth, height: iconHeight }} />
-      </div>
-      <div className="kuvia-icon-card-bottom">
-        <div className="kuvia-icon-card-bottom-content">
-          <p>
-            {text}
-          </p>
+    <div className={`kuvia-icon-card  kuvia-icon-card--${type}`}>
+      <div className="kuvia-icon-card-icon-container">
+        <div className="kuvia-icon-card-icon">
+          <img src={icon} alt={icon} style={{ width: iconWidth, height: iconHeight }} />
         </div>
+        <div className="kuvia-icon-card-badge">{badgeCount}</div>
+      </div>
+      <div className="kuvia-icon-card-content">
+        <p className="kuvia-icon-card-title">{title}</p>
+        <p className="kuvia-icon-card-text">{text}</p>
       </div>
     </div>
   )
