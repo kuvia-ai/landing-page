5a8ecf7e6b456e1a60f86e24f3137a15c678f76a
LucasSon98 <lucas.s.son98@gmail.com>
2025-05-22 22:11:11 -0300

    landing background video pause button

--- src/pages/LandingPage/MainContent/index.tsx
+++ src/pages/LandingPage/MainContent/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useScroll, useTransform } from "framer-motion";
 import background from './background.mp4'
 import Header from "./Header";
@@ -6,6 +6,8 @@ import MotionDiv from "../../../components/MotionDiv";
 import "./index.scss";
 
 const MainContent: React.FC = () => {
+  const [isVideoPaused, setIsVideoPaused] = useState(false);
+
   const { scrollY } = useScroll();
 
   const yMainLandingContent = useTransform(scrollY, [0, 500], [0, -700]);
@@ -47,8 +49,73 @@ const MainContent: React.FC = () => {
             <path d="M75.334 12.591C10.57-24.337-20.852 28.186 15.131 64.566l200.866 209.613c33.472 33.471 46.534 33.471 80.006 0L496.869 64.566c35.983-36.38 4.561-88.903-60.203-51.975L256 109.944 75.334 12.591z" />
           </svg>
         </MotionDiv>
+        <button
+          className='kuvia-landing-background-pause-button'
+          onClick={() => {
+            const video = document.getElementById('kuvia-landing-page-main-content-video') as HTMLVideoElement;
+            if (video) {
+              if (video.paused) {
+                video.play();
+                setIsVideoPaused(false);
+              } else {
+                video.pause();
+                setIsVideoPaused(true);
+              }
+            }
+          }}>
+          {!isVideoPaused ?
+            <svg
+              width="24px"
+              height="24px"
+              viewBox="0 0 512 512"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="#000000"
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <title>ionicons-v5-c</title>
+                <rect
+                  x="176"
+                  y="96"
+                  width="16"
+                  height="320"
+                  style={{ fill: "none", stroke: "#000000", strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: "32px" }}
+                ></rect>
+                <rect
+                  x="320"
+                  y="96"
+                  width="16"
+                  height="320"
+                  style={{ fill: "none", stroke: "#000000", strokeLinecap: "round", strokeLinejoin: "round", strokeWidth: "32px" }}
+                ></rect>
+              </g>
+            </svg>
+            : <svg
+              fill="#000000"
+              width="20px"
+              height="24px"
+              viewBox="0 0 32 32"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              style={{marginRight: "-3px"}}
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <title>play</title>
+                <path
+                  d="M5.92 24.096q0 1.088 0.928 1.728 0.512 0.288 1.088 0.288 0.448 0 0.896-0.224l16.16-8.064q0.48-0.256 0.8-0.736t0.288-1.088-0.288-1.056-0.8-0.736l-16.16-8.064q-0.448-0.224-0.896-0.224-0.544 0-1.088 0.288-0.928 0.608-0.928 1.728v16.16z"
+                ></path>
+              </g>
+            </svg>}
+        </button>
       </MotionDiv>
-    </div>
+    </div >
   );
 };
 
