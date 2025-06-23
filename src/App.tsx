418fe69c36e1809e2635c1453dbfae3b85138b0c
kuvia-ai <info@kuvia.ai>
2025-06-23 13:11:44 +0000

    install vercel Analytics package

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.scss';
 import { AppContextProvider } from './context/AppContext';
 import Container from './components/Container';
 import ApplicationRoutes from './routes';
+import { Analytics } from "@vercel/analytics/react";
 
 const App: React.FC = () => {
   return (
     <AppContextProvider>
       <Container>
         <ApplicationRoutes />
+        <Analytics /> {/* Analytics de Vercel */}
       </Container>
     </AppContextProvider>
   );
