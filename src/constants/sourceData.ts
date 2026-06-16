60759e9510b4da3dc395f8a177da97aa0275ca7a
LucasSon98 <lucas.s.son98@gmail.com>
2026-06-16 00:09:54 -0300

    added new press releases

--- src/constants/sourceData.ts
+++ src/constants/sourceData.ts
@@ -3,6 +3,9 @@ import pressImg2 from '../assets/images/press-images/pressImg2.jpg';
 import pressImg3 from '../assets/images/press-images/pressImg3.jpg';
 import pressImg4 from '../assets/images/press-images/pressImg4.png';
 import pressImg5 from '../assets/images/press-images/pressImg5.jpg';
+import press_image_talentoargen from '../assets/images/press-images/press-image-talentoargen.jpg';
+import press_image_tn_arg from '../assets/images/press-images/press-image-tn-arg.jpeg';
+import press_image_amz_on_air from '../assets/images/press-images/press-image-amz-on-air.jpeg';
 
 import scientificImg1 from '../assets/images/press-images/2e9c4157bd5f05b1906594dfc9191c10d27b3c79.jpg';
 
@@ -44,9 +47,30 @@ export const pressReleasesSource = [
       'Transformar Salud 2025: Celebrating Our Award Showcasing the Scalability of Our AI Tool to Detect BRAF in Pediatric Tumors, in Collaboration with Roche and Fundación Garrahan.',
     URL: 'https://www.transformarsaludfundaciongarrahan.org/ganadores2025-2026',
     date: 'September 12, 2025',
-    img: pressImg5,
+    img: press_image_transformar_salud,
     type: 'Press Release',
   },
+  {
+    description: 'Kuvia Featured by Talento Argentino Showcasing Our Mission to Accelerate Cancer Diagnostics Through Artificial Intelligence.',
+    URL: 'https://x.com/Talentoargen/status/2052801011857699299?s=20',
+    date: 'May 8, 2026',
+    img: press_image_talentoargen,
+    type: 'Press Release',
+  },
+  {
+    description: 'Kuvia Featured on TN Showcasing How AI Can Accelerate Cancer Biomarker Detection and Expand Access to Precision Diagnostics.',
+    URL: 'https://drive.google.com/file/d/1VRrOIi9CA92nLcVn-LZ7aktBIfETJFL9/view?usp=sharing',
+    date: 'May 17, 2026',
+    img: press_image_tn_arg,
+    type: 'Press Release',
+  },
+  {
+    description: 'Kuvia on the AWS On Air Podcast Sharing Our Experience Developing and Deploying AI Solutions for Cancer Diagnostics.',
+    URL: 'https://www.twitch.tv/videos/2767220737?filter=all&sort=time',
+    date: 'May 8, 2026',
+    img: press_image_amz_on_air,
+    type: 'Press Release',
+  }
 ];
 
 export const scientificPublicationsSource = [
